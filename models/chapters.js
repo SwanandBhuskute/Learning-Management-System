d12ac92cdff96ed06ed9677fd2523e7908b0d990
Swanand Bhuskute <swanandbhuskute2003@gmail.com>
2023-11-01 01:05:12 +0530

    Models - Courses and Chapters created

---
+++ models/chapters.js
@@ -0,0 +1,26 @@
+/*eslint-disable no-unused-vars*/
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Chapters extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Chapters.init(
+    {
+      chapterName: DataTypes.STRING,
+      chapterDescription: DataTypes.TEXT,
+    },
+    {
+      sequelize,
+      modelName: "Chapters",
+    },
+  );
+  return Chapters;
+};
