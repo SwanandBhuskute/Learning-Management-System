da5f970b2559f0f81aa1262094e162228d6295bf
Swanand Bhuskute <swanandbhuskute2003@gmail.com>
2023-11-01 17:31:35 +0530

    Association b/w Courses and Teachers

--- models/courses.js
+++ models/courses.js
@@ -10,6 +10,9 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Courses.belongsTo(models.Teachers, {
+        foreignKey: "teacherId",
+      });
     }
   }
   Courses.init(
--- models/teachers.js
+++ models/teachers.js
@@ -10,6 +10,9 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Teachers.hasMany(models.Courses, {
+        foreignKey: "teacherId",
+      });
     }
   }
   Teachers.init(
